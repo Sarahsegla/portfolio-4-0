398a4b70ad75d983c2d5312fef410dd262bb7361
Sarahsegla <sarahsegla@icloud.com>
2023-03-13 15:31:51 +0000

    able to center me

--- portfolio-4-0/src/components/Home/index.js
+++ portfolio-4-0/src/components/Home/index.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 import './Home.css';
-import  Mountain from '../Assets/john-towner-JgOeRuGD_Y4-unsplash.jpg' ;
-import CssBaseline from '@mui/material/CssBaseline';
+// import  Mountain from '../Assets/john-towner-JgOeRuGD_Y4-unsplash.jpg' ;
+// import CssBaseline from '@mui/material/CssBaseline';
 import Me from '../Assets/me.jpg'
-import Container from '@mui/material/Container';
+//import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 
@@ -12,18 +12,11 @@ import Box from '@mui/material/Box';
 function Home(props) {
     return (
         <div className='Home' id='HomeID'>
-           <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="md">
-        <Box>
-        <img className='image' src={Mountain} alt="mountain background" />
+       
         <Box sx={{ flexGrow: 0 }}>
-    <Avatar sx={{  width: '300px', height: '300px', display: 'block'  }} alt="Image of me" src={Me} />
+    <Avatar className="MyAvatar" sx={{ width: '350px', height: '350px', display: 'block'}} alt="Image of me" src={Me} />
     </Box>
-    </Box>
-      </Container>
 
-    </React.Fragment>
 
         </div>
     );
