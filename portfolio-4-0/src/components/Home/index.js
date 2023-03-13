ef3eadc39edc678788fef6806765db3311de7e3c
Sarahsegla <sarahsegla@icloud.com>
2023-03-13 14:18:26 +0000

    added background image for home. added image on me. however no overlay

--- portfolio-4-0/src/components/Home/index.js
+++ portfolio-4-0/src/components/Home/index.js
@@ -1,11 +1,30 @@
 
 import React from 'react';
 import './Home.css';
+import  Mountain from '../Assets/john-towner-JgOeRuGD_Y4-unsplash.jpg' ;
+import CssBaseline from '@mui/material/CssBaseline';
+import Me from '../Assets/me.jpg'
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+
 
 function Home(props) {
     return (
         <div className='Home' id='HomeID'>
-            <h1> Hello Sarah</h1>
+           <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Box>
+        <img className='image' src={Mountain} alt="mountain background" />
+        <Box sx={{ flexGrow: 0 }}>
+    <Avatar sx={{  width: '300px', height: '300px', display: 'block'  }} alt="Image of me" src={Me} />
+    </Box>
+    </Box>
+      </Container>
+
+    </React.Fragment>
+
         </div>
     );
 }
