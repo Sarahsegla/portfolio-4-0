e265eba764198087d6c9ec9928b53a16bb5af35c
Sarahsegla <sarahsegla@icloud.com>
2023-03-14 23:16:53 +0000

    start on project

--- portfolio-4-0/src/components/Contact/index.js
+++ portfolio-4-0/src/components/Contact/index.js
@@ -1,10 +1,13 @@
 import React from 'react' ;
+import './styles.css';
 
 function Contact(props) {
     return (
         <div className='contact' >
         <p>Contact ME:</p>
         <p>Phone Number: 07951350263</p>
+        <p><a href={"https://www.linkedin.com/in/sarah-segla-6788a4203/"} >Linkedin</a></p>
+        <p><a href={"mailto:ablavisarahsegla@icloud.com"}>Ablavisarahsegla@icloud.com</a></p>
         </div>
     )
 }
