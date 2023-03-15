4be6c9fdfe487e70ceebc52dbe0080ba652515c4
Sarahsegla <sarahsegla@icloud.com>
2023-03-15 14:00:36 +0000

    added json to project. got images

--- portfolio-4-0/src/components/Header/index.js
+++ portfolio-4-0/src/components/Header/index.js
@@ -13,7 +13,7 @@ import { Link } from 'react-router-dom';
 
 
 
-const pages = ['Home', 'Project Gallery', 'Project', 'Contact'];
+const pages = ['Home', 'ProjectGallery', 'Project', 'Contact'];
 
 
 
--- portfolio-4-0/src/components/Project/index.js
+++ portfolio-4-0/src/components/Project/index.js
@@ -1,8 +1,27 @@
 import React from 'react' ;
+import MyProject from '../../myProject.json';
 //import ".styles.css";
 
 function Project(props) {
-    return
+    return (
+        <div className='Project'>
+            <h1>My Projects</h1>
+        
+
+        <div className='inside'>
+{
+    MyProject && MyProject.map( Project => {
+        return(
+            <div className='box' key={ Project.id }>
+                <img src={ Project.image } />
+                { Project.caption }
+                </div>
+        )
+    })
+}
+        </div>
+        </div>
+    )
 }
 
 export default Project;
