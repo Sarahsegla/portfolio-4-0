e38990b73c69220b6cc928a093492eb873090e0f
Sarahsegla <sarahsegla@icloud.com>
2023-03-15 00:39:30 +0000

    fixing main home page

--- portfolio-4-0/src/App.js
+++ portfolio-4-0/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react' ;
 import './app.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' ;
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom' ;
 import Home from './components/Home' ;
 import Contact from './components/Contact';
- import Header from './components/Header';
+import Header from './components/Header';
 import Project from './components/Project';
- import ProjectGallery from './components/ProjectGallery';
+import ProjectGallery from './components/ProjectGallery';
 import Wrapper from './components/Wrapper';
 
 
@@ -14,8 +14,9 @@ function App() {
         <Router>
         <>
         <Header />
+        <Home />
         <Wrapper>
-            <Home />
+            
             <Routes>
                 <Route path="/Home" element={<Home />} />
                 <Route path="/ProjectGallery" element={<ProjectGallery />} />
